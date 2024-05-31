38a5079aa1707b39d3dcced7d1a2f9c30648ad42
Ryan Achdiadsyah <achdiadsyah@gmail.com>
2024-05-31 11:05:44 +0700

    Adding minor feature

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,6 +7,12 @@ import contactRoutes from "./contacts";
 import { apiKeyValidator } from "@/middlewares/api-key-validator";
 
 const router = Router();
+router.get("/", (req, res) => {
+    res.json({
+        code: 200,
+        message: "Welcome to the WhatsApp API, Server is up and running!",
+    });
+});
 router.use("/sessions", sessionRoutes);
 router.use("/:sessionId/chats", apiKeyValidator, chatRoutes);
 router.use("/:sessionId/contacts", apiKeyValidator, contactRoutes);
