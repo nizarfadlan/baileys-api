b9ce82a6bc1348b313ba7003305718dc65bf7060
Bhautik Bavadiya <63995315+yesbhautik@users.noreply.github.com>
2024-11-04 12:25:02 +0530

    Update chat.ts

--- src/controllers/chat.ts
+++ src/controllers/chat.ts
@@ -1,66 +1,63 @@
 import type { RequestHandler } from "express";
 import { logger, serializePrisma } from "@/utils";
-// Import PrismaClient and use it to access types
-import { PrismaClient } from "@prisma/client";
+import type { Chat, Message } from "@prisma/client";
 import { prisma } from "@/config/database";
 import { presenceHandler } from "./misc";
-
-// Instantiate PrismaClient
-const prismaClient = new PrismaClient();
+import { PrismaClient} from "@prisma/client";
 
 export const list: RequestHandler = async (req, res) => {
-  try {
-    const { sessionId } = req.params;
-    const { cursor = undefined, limit = 25 } = req.query;
-    const chats = (
-      await prismaClient.chat.findMany({
-        cursor: cursor ? { pkId: Number(cursor) } : undefined,
-        take: Number(limit),
-        skip: cursor ? 1 : 0,
-        where: { sessionId },
-      })
-    ).map((c) => serializePrisma(c));
+	try {
+		const { sessionId } = req.params;
+		const { cursor = undefined, limit = 25 } = req.query;
+		const chats = (
+			await prisma.chat.findMany({
+				cursor: cursor ? { pkId: Number(cursor) } : undefined,
+				take: Number(limit),
+				skip: cursor ? 1 : 0,
+				where: { sessionId },
+			})
+		).map((c: Chat) => serializePrisma(c));
 
-    res.status(200).json({
-      data: chats,
-      cursor:
-        chats.length !== 0 && chats.length === Number(limit)
-          ? chats[chats.length - 1].pkId
-          : null,
-    });
-  } catch (e) {
-    const message = "An error occurred during chat list";
-    logger.error(e, message);
-    res.status(500).json({ error: message });
-  }
+		res.status(200).json({
+			data: chats,
+			cursor:
+				chats.length !== 0 && chats.length === Number(limit)
+					? chats[chats.length - 1].pkId
+					: null,
+		});
+	} catch (e) {
+		const message = "An error occured during chat list";
+		logger.error(e, message);
+		res.status(500).json({ error: message });
+	}
 };
 
 export const find: RequestHandler = async (req, res) => {
-  try {
-    const { sessionId, jid } = req.params;
-    const { cursor = undefined, limit = 25 } = req.query;
-    const messages = (
-      await prismaClient.message.findMany({
-        cursor: cursor ? { pkId: Number(cursor) } : undefined,
-        take: Number(limit),
-        skip: cursor ? 1 : 0,
-        where: { sessionId, remoteJid: jid },
-        orderBy: { messageTimestamp: "desc" },
-      })
-    ).map((m) => serializePrisma(m));
+	try {
+		const { sessionId, jid } = req.params;
+		const { cursor = undefined, limit = 25 } = req.query;
+		const messages = (
+			await prisma.message.findMany({
+				cursor: cursor ? { pkId: Number(cursor) } : undefined,
+				take: Number(limit),
+				skip: cursor ? 1 : 0,
+				where: { sessionId, remoteJid: jid },
+				orderBy: { messageTimestamp: "desc" },
+			})
+		).map((m: Message) => serializePrisma(m));
 
-    res.status(200).json({
-      data: messages,
-      cursor:
-        messages.length !== 0 && messages.length === Number(limit)
-          ? messages[messages.length - 1].pkId
-          : null,
-    });
-  } catch (e) {
-    const message = "An error occurred during chat find";
-    logger.error(e, message);
-    res.status(500).json({ error: message });
-  }
+		res.status(200).json({
+			data: messages,
+			cursor:
+				messages.length !== 0 && messages.length === Number(limit)
+					? messages[messages.length - 1].pkId
+					: null,
+		});
+	} catch (e) {
+		const message = "An error occured during chat find";
+		logger.error(e, message);
+		res.status(500).json({ error: message });
+	}
 };
 
 export const presence = presenceHandler();
